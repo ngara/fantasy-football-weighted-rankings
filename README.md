73e858ec83459d68e11628205813c37056dfab75
Nathan Garabedian <ngara23@gmail.com>
2020-08-31 06:51:56 -0700

    Updated data for 2020 from fantasyfootballcalculator.com

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 How to get data:
 
+* go to https://fantasyfootballcalculator.com/adp
+* Choose Draft, and then PPR ADP, Half-PPR ADP, Non-PPR ADP, or 2-QB ADP from the left side.
+* Go to Display -> JSON to download the data for that year.  Name it:
+  <YEAR>-ppr.json
+  <YEAR>-halfppr.json
 
-Yahoo
------
-Register for Yahoo Developer Network and get your credentials:
-3.1) Register for a Yahoo account at https://yahoo.com/
-3.2) Go to https://developer.yahoo.com/ --> 'My Apps' --> 'YDN Apps'
-3.3) On the lefthand panel, click 'Create an App'
-3.4) Name your app whatever you please ("Fantasy Football Stats") in the 'Application Name' block.
-3.5) Select the 'Installed Application' option since we're only going to be accessing the data from our local machines.
-3.6) Under the 'API Permissions' sections select 'Fantasy Sports' and then make sure that 'Read' is selected. 'Read/Write' would only be used if you wanted to be able to control your league via Python scripting vice just reading the data from your league. You can come back and change these options in the future.
-3.7) The redirect uri can be https://oob for out of band meaning no uri
-3.7) You have now successfully created a Yahoo Developer App and you will see App ID, Client ID(Consumer Key), and Client Secret(Consumer Secret) with a long string of random letters and numbers. Yahoo will use these keys to allow you to access it's API via OAuth 2.0.
-
+* Update bin/picks.py to reflect information you know (i.e. this_year variable)
+* run it to get the overall weighted rankings you've chosen, or grep out the position of your choice
--- bin/picks.py
+++ bin/picks.py
@@ -1,65 +1,70 @@
 #!/usr/bin/env python
 # Download data in json form from:
-#
+# https://partners.fantasypros.com/api/v1/consensus-rankings.php?sport=NFL&year=2020&week=0&id=1054&position=ALL&type=ST&scoring=HALF&filters=7:9:285:699:747&export=csv
+#  (found through yahoo fantasy football - free download)
 #
 import os
+import sys
 import json
 import fnmatch
 
 mydir = os.path.dirname(__file__)
-datapath = os.path.abspath(os.path.join(mydir, '../var/data'))
+datapath = os.path.abspath(os.path.join(mydir, "../var/data"))
 data = {}
+this_year = "2020"
 
 # The ranking values for each year
 w_lookup = {
-    '2020': 100,
-    '2019': 65,
-    '2018': 45,
-    '2017': 44,
-    '2016': 43,
+    "2020": 100,
+    "2019": 55,
+    "2018": 1,
 }
 
 # Players not to choose this season (due to injuries, or maybe you just don't
 # want to use this player this year)
-skip = [
-    #"Jay Ajayi",
-    #"Carson Wentz",
-    #"Alshon Jeffery",
-    #"Josh Gordon",
-    #"Jerick McKinnon",
-    #"Evan Engram", # week1?
-    #"Marqise Lee",
-    #"DeVante Parker",
-    #"Nelson Agholor",
+injuries = [
+    "David Montgomery",
+    "Joe Mixon",
+    # "Alshon Jeffery",
+    # "Josh Gordon",
+    # "Jerick McKinnon",
+    # "Evan Engram", # week1?
+    # "Marqise Lee",
+    # "DeVante Parker",
+    # "Nelson Agholor",
 ]
 
-
 # Walk data path and read all .csv filenames
-datafiles = [os.path.join(dirpath, f) \
-             for dirpath, dirnames, files in os.walk(datapath) \
-             for f in fnmatch.filter(files, '*.json')]
-
+datafiles = [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk(datapath)
+    for f in fnmatch.filter(files, "*.json")
+]
 
 # read files
 # {u'position': u'RB', u'name': u'Todd Gurley', u'adp': 1.5, u'times_drafted': 285, u'stdev': 0.7, u'high': 1, u'low': 4, u'team': u'LAR', u'player_id': 2280, u'adp_formatted': u'1.02', u'bye': 12}
 for f in datafiles:
     # print "Processing %s" % f
-    with open(f, 'r') as _fp:
+    with open(f, "r") as _fp:
         _d = json.load(_fp)
-    for p in _d['players']:
-        name = p['name']
-        year = f.split('/')[-1].split('-')[0]
-        ppr = f.split('/')[-1].split('.')[0].split('-')[1]
-        adp = p['adp']
-        times_drafted = p['times_drafted']
-        team = p['team']
-        high = p['high']
-        low = p['low']
-        adp_formatted = p['adp_formatted']
-        player_id = p['player_id']
-        position = p['position']
-        bye = p['bye']
-        stdev = p['stdev']
+
+    players = _d["players"]
+    for p in players:
+        name = p["name"]
+
+        year = f.split("/")[-1].split("-")[0]
+        ppr = f.split("/")[-1].split(".")[0].split("-")[1]
+
+        adp = p["adp"]
+        times_drafted = p["times_drafted"]
+        team = p["team"]
+        position = p["position"]
+        bye = p["bye"]
+        high = p["high"]
+        low = p["low"]
+        adp_formatted = p["adp_formatted"]
+        player_id = p["player_id"]
+        stdev = p["stdev"]
 
         data[name] = data.get(name, {})
         data[name][year] = data[name].get(year, {})
@@ -68,7 +73,8 @@ for f in datafiles:
 
 final_list = []
 for name in data.keys():
-    if data[name].get('2018', None) is None:
+    # If the player doesn't exist in this year's roster, skip them
+    if data[name].get(this_year, None) is None:
         continue
 
     # Add year weights to ADP
@@ -76,7 +82,7 @@ for name in data.keys():
     for year in data[name].keys():
         adp_ppr = []
         for ppr in data[name][year].keys():
-            adp_ppr.append(data[name][year][ppr]['adp'])
+            adp_ppr.append(data[name][year][ppr]["adp"])
         adp_ppr_final = sum(adp_ppr) / float(len(adp_ppr))
 
         weight = w_lookup[year]
@@ -84,19 +90,20 @@ for name in data.keys():
             weighted_adp.append(adp_ppr_final)
     weighted_adp_final = sum(weighted_adp) / float(len(weighted_adp))
 
-    position = data[name]['2018']['halfppr']['position']
-    bye = data[name]['2018']['halfppr']['bye']
+    position = data[name][this_year]["halfppr"]["position"]
+    bye = data[name][this_year]["halfppr"]["bye"]
     final_list.append([position, name, weighted_adp_final, bye])
 
-final_list.sort(key = lambda x: x[2])
+final_list.sort(key=lambda x: x[2])
 
 line_no = 0
 for li in final_list:
     line_no += 1
-    if li[0] in skip:
-        inj = '(I)'
+    if li[0] in injuries:
+        inj = "(I)"
     else:
-        inj = ''
-    print("%3s %4s %20s %7.2f  (%i) %4s" % (
-        line_no, li[0], li[1], li[2], li[3], inj))
-
+        inj = ""
+    print(
+        "%3s %4s %20s %7.2f  (%i) %4s"
+        % (line_no, li[0], li[1], li[2], li[3], inj)
+    )
